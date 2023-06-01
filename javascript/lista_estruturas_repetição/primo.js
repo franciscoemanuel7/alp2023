5a1c076949bdc342e3126308776d41b9d9008362
Francisco Emanuel <129955682+franciscoemanuel7@users.noreply.github.com>
2023-06-01 07:20:47 -0300

    lista_estruturas_repetiçao

--- javascript/lista_estruturas_repetição/primo.js
+++ javascript/lista_estruturas_repetição/primo.js
@@ -4,10 +4,8 @@ if (n==2){
     document.write("O número é primo!");
 }
 else if (n>2){
-    //let divisores = 0;
     for (let i = 2; i<=n; i++){
         if (n%i==0){
-            //divisores++;
             ePrimo = false;
             break;
         }
