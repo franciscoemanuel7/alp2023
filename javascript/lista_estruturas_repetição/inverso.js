205cebc104caada31772b89f374c511b91bc5cda
Francisco Emanuel <129955682+franciscoemanuel7@users.noreply.github.com>
2023-06-06 19:23:24 -0300

    lista_estruturas_repetição

--- javascript/lista_estruturas_repetição/inverso.js
+++ javascript/lista_estruturas_repetição/inverso.js
@@ -1,3 +1,10 @@
-n = Number(prompt("Digite um número:"))
-i = n * (-1);
-document.write(`O número inverso de ${n} é ${i}!`);
+let n = Number(prompt("Digite um número inteiro positivo:"));
+let inv = 0;
+
+while (n > 0) {
+  let digito = n % 10;
+  inv = inv * 10 + digito;
+  n = Math.floor(n / 10);
+}
+
+document.write(`O número invertido é: ${inv}`);
